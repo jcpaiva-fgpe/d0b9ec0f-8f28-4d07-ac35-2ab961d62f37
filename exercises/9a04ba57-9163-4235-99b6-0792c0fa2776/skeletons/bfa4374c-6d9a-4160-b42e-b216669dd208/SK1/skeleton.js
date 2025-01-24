5f84257debf9396cdee6a837ffa6aa83b670c321
Yannik Bauer <yannikbauer.1@gmail.com>
2025-01-24 09:16:25 +0000

    created file exercises/9a04ba57-9163-4235-99b6-0792c0fa2776/skeletons/bfa4374c-6d9a-4160-b42e-b216669dd208/SK1/skeleton.js

---
+++ exercises/9a04ba57-9163-4235-99b6-0792c0fa2776/skeletons/bfa4374c-6d9a-4160-b42e-b216669dd208/SK1/skeleton.js
@@ -0,0 +1,16 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+function main() {
+    const n = parseInt(readln(), 10);
+    writeln(oddishOrEvenish(+n));
+}
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+
+function oddishOrEvenish(n) {
+	// TODO this is what you need to complete
+}
